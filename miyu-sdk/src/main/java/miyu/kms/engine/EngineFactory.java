922f3f4cc5157384a2e86d807e64a37aa91e6591
xudean <xuda.it@outlook.com>
2022-08-30 13:46:04 +0800

    feat(miyu-sdk):  Add engine factory

--- miyu-sdk/src/main/java/miyu/kms/engine/EngineFactory.java
+++ miyu-sdk/src/main/java/miyu/kms/engine/EngineFactory.java
@@ -1,10 +1,20 @@
 package miyu.kms.engine;
 
+import miyu.kms.constants.EngineType;
+import miyu.kms.engine.impl.RSAFileEngine;
+
 /**
  * @author : xudean
  * @version V1.0
- * @Description: TODO
+ * @Description: 先做一个简单工厂
  * @date Date : 2022年08月26日 上午11:46
  */
 public class EngineFactory {
+    public IEngine init(EngineType type){
+        switch (type){
+            case FILE:
+                return new RSAFileEngine();
+        }
+        throw new RuntimeException("unsuported engineType:"+type.name());
+    }
 }
